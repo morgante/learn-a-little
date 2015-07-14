81203d9645149341f5d072f05c9b838ce44289f9
Morgante Pell <morgante.pell@morgante.net>
2015-07-14 02:35:15 -0400

    make the solution in O(n) time

--- integers/index.js
+++ integers/index.js
@@ -5,13 +5,16 @@
 function get_products_of_all_ints_except_at_index(numbers) {
 	var products = [];
 	var before = 1;
-	for (i = 0; i < numbers.length; i++) {
-		var after = 1;
-		for (n = i + 1; n < numbers.length; n++) {
-			after = after * numbers[n];
-		}
-		products.push(before * after);
+	for (var i = 0; i < numbers.length; i++) {
+		products.push(before);
 		before = before * numbers[i];
+	}
+
+	var after = 1;
+	for (var j = numbers.length - 1; j >= 0; j--) {
+		products[j] = products[j] * after;
+		after = after * numbers[j];
+
 	}
 
 	return products;
