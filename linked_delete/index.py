dfb80ed45cff2229c49c22735676e570d31a991a
Morgante Pell <morgante.pell@morgante.net>
2015-07-16 14:31:36 -0400

    linked_delete: make the function work on last node

--- linked_delete/index.py
+++ linked_delete/index.py
@@ -5,14 +5,16 @@ https://www.interviewcake.com/question/delete-node
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
+
+        if (self.value != None):
+            self.next = Node(None)
 
 def print_list(head):
     print "printing list!"
 
     node = head
     print node.value
-    while (node.next != None):
+    while (node.next.value != None):
         node = node.next
         print node.value
 
@@ -24,17 +26,24 @@ def delete_node(node):
         node.value = node.next.value
         node.next = node.next.next
     except:
-        raise Exception("cannot delete last node")
+        node.value = None
+        # raise Exception("cannot delete last node")
 
 a = Node('A')
 b = Node('B')
 c = Node('C')
+d = Node('D')
 
 a.next = b
 b.next = c
+c.next = d
 
 print_list(a)
 
 delete_node(b)
+
+print_list(a)
+
+delete_node(d)
 
 print_list(a)
