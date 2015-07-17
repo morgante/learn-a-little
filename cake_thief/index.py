12acb0fafc3af3d4e1d33c179e06443d9c782317
Morgante Pell <morgante.pell@morgante.net>
2015-07-17 01:31:59 -0400

    cake thief: greedier algorithm

--- cake_thief/index.py
+++ cake_thief/index.py
@@ -5,29 +5,31 @@ https://www.interviewcake.com/question/cake-thief
 class Bag:
     def __init__(self):
         self.cakes = []
-    def add_cake(self, cake):
+    def add(self, cake):
         self.cakes.append(cake)
     def get_weight(self):
         return reduce(lambda memo, cake: memo + cake[0], self.cakes, 0)
+    def get_value(self):
+        return reduce(lambda memo, cake: memo + cake[1], self.cakes, 0)
 
 def max_duffel_bag_value(cakes, capacity):
+    if len(cakes) < 1:
+        return 0
+
     options = []
 
-    for cake in cakes:
-        added = 0
-        while added * cake[0] <= capacity:
-            bag = Bag()
-            for x in range(0, added):
-                bag.add_cake(cake)
-            options.append(bag)
-            added += 1
+    cakes.sort(key=lambda cake: cake[1] / cake[0], reverse=True)
 
-    for bag in options:
-        print bag.cakes
+    bag = Bag()
 
+    while bag.get_weight() <= capacity - cakes[0][0]:
+        bag.add(cakes[0])
 
+    another_bag = max_duffel_bag_value(cakes[1:], capacity - bag.get_weight())
+
+    return another_bag + bag.get_value()
 
 cake_tuples = [(7, 160), (3, 90), (2, 15)]
-capacity    = 20
+capacity = 20
 
-max_duffel_bag_value(cake_tuples, capacity)
+print max_duffel_bag_value(cake_tuples, capacity)
