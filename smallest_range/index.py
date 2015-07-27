0341ed7ad1367e5bec42095074b819da8a3efd6f
Morgante Pell <morgante.pell@morgante.net>
2015-07-27 01:07:13 -0400

    smallest_range: set up problem

---
+++ smallest_range/index.py
@@ -0,0 +1,11 @@
+'''
+http://www.careercup.com/question?id=16759664
+
+Find the smallest range which includes at least one number from each list
+
+'''
+
+def smallest_sample(lists):
+    return (20, 24)
+
+print smallest_sample([4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30])
