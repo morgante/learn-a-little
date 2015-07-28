2db6760af4327b9cd0890090f0efe46bdbe8a2fe
Morgante Pell <morgante.pell@morgante.net>
2015-07-28 02:28:51 -0400

    merge_ranges: solve merged meeting ranges problem

--- merge_ranges/index.py
+++ merge_ranges/index.py
@@ -3,6 +3,21 @@ https://www.interviewcake.com/question/merging-ranges
 '''
 
 def condense_meeting_times(meetings):
-    return [(0, 1), (3, 8), (9, 12)]
+    # first, sort all our meetings
+    sorted_meetings = sorted(meetings)
 
-print condense_meeting_times([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+    new_meetings = [meetings[0]]
+
+    for meeting_start, meeting_end in sorted_meetings:
+        last_meeting_start, last_meeting_end = new_meetings[-1]
+
+        if (meeting_start <= last_meeting_end):
+            new_meetings[-1] = (last_meeting_start, max(meeting_end, last_meeting_end))
+        else:
+            new_meetings.append((meeting_start, meeting_end))
+
+    return new_meetings
+
+print condense_meeting_times([(0, 1), (4, 8), (10, 12), (9, 10), (3, 5)])
+
+print condense_meeting_times([(1, 10), (2, 6), (3, 5), (7, 9)])
