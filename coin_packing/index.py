b0c4958943789ba9296f752496c3cc0fd23e87ab
Morgante Pell <morgante.pell@morgante.net>
2015-07-28 02:58:15 -0400

    coin_packing: top-down solution

--- coin_packing/index.py
+++ coin_packing/index.py
@@ -2,10 +2,23 @@
 https://www.interviewcake.com/question/coin
 '''
 
-def get_ways(amount, denominations):
-    return [(1, 1, 1, 1), (1, 1, 2), (1, 3), (2, 2)]
-
 def count_ways(amount, denominations):
-    return len(get_ways(amount, denominations))
+    if amount is 0:
+        return 1
+    elif amount < 0:
+        return 0
+    elif len(denominations) is 0:
+        return 0
+
+    print "checking it", amount, denominations
+
+    coin = denominations[0]
+    count = 0
+
+    while amount >= 0:
+        count += count_ways(amount, denominations[1:])
+        amount -= coin
+
+    return count
 
 print count_ways(4, (1, 2, 3))
