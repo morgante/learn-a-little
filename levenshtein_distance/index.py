1251f2331170f3db4e1ed065a42f2dfe73ca7d09
Morgante Pell <morgante.pell@morgante.net>
2015-07-29 01:56:25 -0400

    fix first test case

--- levenshtein_distance/index.py
+++ levenshtein_distance/index.py
@@ -8,7 +8,13 @@ Only 3 operation types are allowed:
 '''
 
 def count_ops(a, b):
-    return 2
+    op_count = 0
 
-assert count_ops("a", "ab") is 2
+    if len(a) < len(b):
+        op_count += abs(len(b) - len(a))
+
+    print op_count
+    return op_count
+
+assert count_ops("a", "ab") is 1
 assert count_ops("kitten", "sitting") is 3
