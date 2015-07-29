892aa9a7c6857b28be7647e9341c0cf9655f182f
Morgante Pell <morgante.pell@morgante.net>
2015-07-29 02:39:47 -0400

    fill in a matrix with something

--- levenshtein_distance/index.py
+++ levenshtein_distance/index.py
@@ -8,18 +8,26 @@ Only 3 operation types are allowed:
 '''
 
 def count_ops(a, b):
-    if a is "":
-        return len(b)
-    elif b is "":
-        return len(a)
+    # create a matrix for our distances
+    matrix = []
 
-    if a[-1] == b[-1]:
-        op = 0
-    else:
-        op = 1
+    # fill in the first row of the matrix
+    matrix.append([x for x in range(0, len(b))])
 
-    return min(count_ops(a, b[:-1]) + 1, count_ops(a[:-1], b) + 1, count_ops(a[:-1], b[:-1]) + op)
+    for row_number in xrange(1, len(a)):
+        row = [matrix[row_number - 1][0] + 1]
+        matrix.append(row)
 
-print count_ops("cb", "ab"), 1
-print count_ops("a", "ab"), 1
+        for col_number in xrange(1, len(b)):
+            top = matrix[row_number - 1][col_number]
+            left = row[col_number - 1]
+
+            row.append(min(top, left) + 1)
+
+    print matrix
+
+    return 1
+
+# print count_ops("cb", "ab"), 1
+# print count_ops("a", "ab"), 1
 print count_ops("kitten", "sitting"), 3
