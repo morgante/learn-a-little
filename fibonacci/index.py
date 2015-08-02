b6df9a074641a768aadb64688ff6dcb545344486
Morgante Pell <morgante.pell@morgante.net>
2015-08-02 02:41:10 -0400

    fibonacci: try a closed form solution, doesnt work correctly

--- fibonacci/index.py
+++ fibonacci/index.py
@@ -3,17 +3,15 @@ https://www.interviewcake.com/question/nth-fibonacci
 '''
 
 def n_fib(n):
-    last_two = (0, 1)
-    if (n in last_two):
-        return n
-    i = len(last_two)
-
-    while i <= n:
-        new_value = sum(last_two)
-        last_two = (last_two[1], new_value)
-        i += 1
-
-    return last_two[1]
+    matrix = ((1, 1), (1, ))
+    for result in bin(n)[3:]:
+        intermediate = matrix[0][0]*matrix[0][0]
+        matrix = ((matrix[0][0]*matrix[0][0] + intermediate,
+                   (matrix[0][0] + matrix[1][0]) * matrix[0][1]),
+                  (matrix[1][0]*matrix[1][0] + intermediate, ))
+        if result is '1':
+            matrix = ((matrix[0][0] + matrix[0][1], matrix[0][0]), (matrix[0][1], ))
+    return matrix[0][1]
 
 for n in range(0, 10):
     print n, n_fib(n)
