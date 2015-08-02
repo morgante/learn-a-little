89730239a22362c8f70cada0c53e25e585d8daf6
Morgante Pell <morgante.pell@morgante.net>
2015-08-02 02:19:50 -0400

    fibonacci: make solution a little more elegant

--- fibonacci/index.py
+++ fibonacci/index.py
@@ -3,12 +3,10 @@ https://www.interviewcake.com/question/nth-fibonacci
 '''
 
 def n_fib(n):
-    if (n == 0):
-        return 0
-    elif (n <= 2):
-        return 1;
-    last_two = (1, 1)
-    i = 3
+    last_two = (0, 1)
+    if (n in last_two):
+        return n
+    i = len(last_two)
 
     while i <= n:
         new_value = sum(last_two)
