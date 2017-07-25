3a1acb46ae6b88286a77a7b7d289ec6aded4cbac
Morgante Pell <morgante.pell@morgante.net>
2017-07-25 16:07:51 -0700

    Solve finding the nth largest element in BST

--- second-largest-bst/index.py
+++ second-largest-bst/index.py
@@ -48,5 +48,27 @@ def build_tree(depth, min_value, max_value):
         node.right = build_tree(depth - 1, value + 1, max_value)
     return node
 
+def find_largest(tree):
+    current = tree
+    while current:
+        if not current.right:
+            return current
+        current = current.right
+
+def find_nth_largest(tree, n):
+    if n != 2:
+        raise Exception("I can only handle find the second largest")
+    node = tree
+    while node:
+        if node.right and not node.right.left and not node.right.right:
+            return node
+        elif node.left and not node.right:
+            return find_largest(node.left)
+        else:
+            node = node.right
+
 tree = build_tree(5, 3, 1000)
 tree.print_tree()
+
+largest = find_nth_largest(tree, 2)
+print('largest', largest.value)
