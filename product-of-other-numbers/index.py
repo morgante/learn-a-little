9b0ba496e3db6721c7d6be00f13f0d0a79c88b51
Morgante Pell <morgante.pell@morgante.net>
2017-08-11 03:43:17 -0400

    Simplify product of other ints to only use one new list

--- product-of-other-numbers/index.py
+++ product-of-other-numbers/index.py
@@ -6,27 +6,28 @@ Write a function get_products_of_all_ints_except_at_index() that takes a list of
 Do not use division in your solution.
 '''
 
+'''
+Solution analysis:
+
+time complexity: O(n)
+space complexity: O(n)
+'''
+
 def get_products_of_other_ints(ints):
     left_product = 1
-    right_product = 1
-    left_products = [1] * len(ints)
-    right_products = [1] * len(ints)
-    final_products = [0] * len(ints)
+    final_products = [1] * len(ints)
 
     for i in range(1, len(ints)):
-        left_val = ints[i - 1]
-        right_val = ints[len(ints) - i]
-        print('analysis', left_val, right_val)
+        left_product *= ints[i - 1]
+        final_products[i] = left_product
 
-        left_product *= left_val
-        left_products[i] = left_product
-
-        right_product *= right_val
-        right_products[len(ints) - i - 1] = right_product
-
-    for i in range(0, len(ints)):
-        final_products[i] = left_products[i] * right_products[i]
+    right_product = 1
+    for j in range(0, len(ints)):
+        right_index = len(ints) - j - 1
+        final_products[right_index] *= right_product
+        right_product *= ints[right_index]
 
     return final_products
 
 print(get_products_of_other_ints([1, 7, 3, 4]), 'should be', [84, 12, 28, 21], 'for', [1, 7, 3, 4])
+print(get_products_of_other_ints([2, 0, 2]))
