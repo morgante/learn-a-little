bc64f7fa3813cb94adec4efe238dd486f15cc84e
Morgante Pell <morgante.pell@morgante.net>
2017-08-16 01:31:24 -0400

    Add iterative solution for finding rotation point

--- find_rotation_point/index.py
+++ find_rotation_point/index.py
@@ -2,7 +2,7 @@
 https://www.interviewcake.com/question/python/find-rotation-point
 '''
 
-def find_rotation_point(words, first_value=None, front_i=0):
+def find_rotation_point_recursive(words, first_value=None, front_i=0):
     if first_value is None:
         first_value = words[0]
 
@@ -10,25 +10,41 @@ def find_rotation_point(words, first_value=None, front_i=0):
     pivot = words[pivot_i]
     previous_value = words[pivot_i - 1]
 
-    print('compare', pivot, first_value, previous_value)
-
     if previous_value >= pivot:
         return front_i + pivot_i
 
     if first_value < pivot:
         # We are too far to the front
-        return find_rotation_point(
+        return find_rotation_point_recursive(
             words[pivot_i:],
             first_value=first_value,
             front_i=(pivot_i + front_i)
         )
 
     # We are too far to the end
-    return find_rotation_point(
+    return find_rotation_point_recursive(
         words[:pivot_i],
         first_value=first_value,
         front_i=front_i
     )
+
+def find_rotation_point_iterative(words):
+    left_bound = 0
+    right_bound = len(words) - 1
+    first_value = words[0]
+
+    while left_bound < right_bound:
+        pivot_i = left_bound + int((right_bound - left_bound) / 2)
+        pivot = words[pivot_i]
+        previous_value = words[pivot_i - 1]
+
+        if previous_value >= pivot:
+            return pivot_i
+
+        if first_value < pivot:
+            left_bound = pivot_i
+        else:
+            right_bound = pivot_i
 
 words = [
     'ptolemaic',
@@ -42,4 +58,4 @@ words = [
     'engender',
 ]
 
-print('comapre', find_rotation_point(words), words.index('asymptote'))
+print('comapre', find_rotation_point_recursive(words), find_rotation_point_iterative(words), words.index('asymptote'))
