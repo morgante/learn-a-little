810994b042ba7165841fb9461bf016b6fb56461b
Morgante Pell <morgante.pell@morgante.net>
2017-08-16 00:28:10 -0400

    Make balanced search tree short circuit if it encounters a big depth difference

--- balanced_binary_tree/index.py
+++ balanced_binary_tree/index.py
@@ -10,7 +10,7 @@ python3 -m balanced_binary_tree.index
 
 from common.bst import BinaryTreeNode, build_tree
 
-def get_max_leaf_difference(tree):
+def get_max_leaf_difference(tree, max_difference=None):
     to_traverse = [(0, tree)]
     min_depth = None
     max_depth = 0
@@ -20,6 +20,9 @@ def get_max_leaf_difference(tree):
         if not node.left and not node.right:
             min_depth = min(min_depth, current_depth) if min_depth is not None else current_depth
             max_depth = max(max_depth, current_depth)
+
+            if max_difference and max_depth - min_depth > max_difference:
+                return max_depth - min_depth
             continue
         if node.left:
             to_traverse.append((current_depth + 1, node.left))
@@ -29,16 +32,16 @@ def get_max_leaf_difference(tree):
     return max_depth - min_depth
 
 def is_superbalanced(tree):
-    diff = get_max_leaf_difference(tree)
+    diff = get_max_leaf_difference(tree, 1)
     if diff <= 1:
         return True
     return False
 
-my_tree = build_tree(14, 5, 200)
+my_tree = build_tree(7, 5, 50)
 
 # my_tree.left.left.left = None
 # my_tree.left.left.right = None
 
-print('is superbalanced?', get_max_leaf_difference(my_tree))
-
 my_tree.print_tree()
+
+print('is superbalanced?', get_max_leaf_difference(my_tree), is_superbalanced(my_tree))
