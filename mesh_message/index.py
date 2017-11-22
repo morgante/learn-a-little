caa8edefd338ae78887c5cbad4f080deac311a8c
Morgante Pell <morgante.pell@morgante.net>
2017-11-21 21:28:14 -0500

    [mesh_message] first iteration (not super efficient)

---
+++ mesh_message/index.py
@@ -0,0 +1,43 @@
+'''
+https://www.interviewcake.com/question/python/mesh-message
+'''
+
+def find_shortest_path(network, origin, destination, visited=None):
+    if visited is None:
+        visited = [origin]
+    else:
+        visited = visited + [origin, ]
+
+    try:
+        neighbors = network[origin]
+    except KeyError:
+        return None
+
+    if destination in neighbors:
+        return visited + [destination, ]
+
+    shortest_path = None
+    for neighbor in neighbors:
+        if neighbor not in visited:
+            new_path = find_shortest_path(network, neighbor, destination, visited)
+            if new_path:
+                if not shortest_path:
+                    shortest_path = new_path
+                else:
+                    if len(new_path) < len(shortest_path):
+                        shortest_path = new_path
+
+    return shortest_path
+
+network = {
+    'Min'     : ['William', 'Jayden', 'Omar'],
+    'William' : ['Min', 'Noam'],
+    'Jayden'  : ['Min', 'Amelia', 'Ren', 'Noam'],
+    'Ren'     : ['Jayden', 'Omar'],
+    'Amelia'  : ['Jayden', 'Adam', 'Miguel'],
+    'Adam'    : ['Amelia', 'Miguel', 'Sofia', 'Lucas'],
+    'Miguel'  : ['Amelia', 'Adam', 'Liam', 'Nathan'],
+}
+
+# print(find_shortest_path(network, 'Jayden', 'Adam'))
+print(find_shortest_path(network, 'Miguel', 'Omar'))
